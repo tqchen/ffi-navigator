fb813d0c97239285a04ed79e59ccdae2b3629eb1
tqchen <tianqi.tchen@gmail.com>
2019-12-28 09:45:11 -0800

    add python setup

---
+++ python/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+long_description = open("../README.md").read()
+
+setup(
+    name = "ffi-navigator",
+    version = "0.1",
+    license="Apache-2.0",
+    description = "Language server for navigating FFI calls",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = "tvm community",
+    author_email = "tianqi.tchen@gmail.com",
+    url = "https://github.com/tqchen/ffi-navigator",
+    keywords = [],
+    packages = find_packages(),
+    install_requires = [
+        "python-jsonrpc-server",
+        "attrs"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+  ],
+)
