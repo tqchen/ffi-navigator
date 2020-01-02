62e812eb608bca9bc6878c166424bcbbae3d58d4
Masahiro Masuda <masahi129@gmail.com>
2020-01-02 17:36:04 +0900

    add nop provider

--- python/ffi_navigator/dialect/__init__.py
+++ python/ffi_navigator/dialect/__init__.py
@@ -4,8 +4,33 @@ from .tvm import TVMProvider
 from .mxnet import MXNetProvider
 
 
+class NoOpProvider:
+    """A provider for projects without a registered dialect.
+    """
+    def __init__(self, resolver, logger):
+        self.resolver = resolver
+        self.logger = logger
+
+    def _cc_extract(self, path, source, begin, end):
+        return []
+
+    def _py_extract(self, path, source, begin, end):
+        return []
+
+    def init_pass(self, path, source):
+        pass
+
+    def extract(self, path, source, begin=0, end=None):
+        return []
+
+    def extract_symbol(self, path, source, pos):
+        return None
+
+
 def create_dialect(root_path, resolver, logger):
     if os.path.exists(os.path.join(root_path, "python", "tvm")):
         return TVMProvider(resolver, logger)
     elif os.path.exists(os.path.join(root_path, "python", "mxnet")):
         return MXNetProvider(resolver, logger)
+    else:
+        return NoOpProvider(resolver, logger)
