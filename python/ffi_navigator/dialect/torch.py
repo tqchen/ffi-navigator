4db61caca15bf3e661f4c143ff551d9445072e70
Masahiro Masuda <masahi129@gmail.com>
2020-01-03 18:22:46 +0900

    cast int64 from cumsum to int

--- python/ffi_navigator/dialect/torch.py
+++ python/ffi_navigator/dialect/torch.py
@@ -7,30 +7,28 @@ from .. import pattern
 from ..lsp import Range, Position
 
 
-def _re_match_multi_line(pat, prefix, path, lines, logger):
+def _re_match_multi_line(pat, prefix, path, lines):
     source = "\n".join(lines)
-    matches = list(re.finditer(r"\.def\((?P<key_space>\s*)\"(?P<key_func>[a-z0-9|_]+)\"", source))
+    matches = list(re.finditer(pat, source))
     if matches == []:
         return []
-    line_count = len(lines)
     cumsum = np.cumsum(list(map(lambda line: len(line)+1, lines))) # +1 for newline
 
     next_begin = 0
     result = []
     # find line num, start and end pos for each match
     for match in matches:
-        line_num = bisect(cumsum[next_begin:], match.start()) + next_begin
+        line_num = int(bisect(cumsum[next_begin:], match.start())) + next_begin
         next_begin = line_num
         line_num_start = line_num
         line_num_end = line_num
         if match.group("key_space"):
             line_num_end += 1
-        pos_start = match.start() - cumsum[line_num_start-1]
-        pos_end = match.end() - cumsum[line_num_end-1]
+        pos_start = match.start() - int(cumsum[line_num_start-1])
+        pos_end = match.end() - int(cumsum[line_num_end-1])
         rg = Range(Position(line_num_start, pos_start), Position(line_num_end, pos_end))
         key = prefix + match.group("key_func")
         result.append(pattern.Def(key=key, path=path, range=rg))
-        logger.info("key=%s, line_num=%d, span=(%d, %d)", key, line_num, match.start(), match.end())
 
     return result
 
@@ -60,7 +58,7 @@ class TorchProvider:
             pattern.Def(key=match.group("key"), path=path, range=rg),
             use_search=True)
         self.cpp_pybind_func = lambda path, lines: \
-          _re_match_multi_line(r"\.def\(\s*\"(?P<key_func>[a-z0-9|_]+)\"", "", path, lines, self.logger)
+          _re_match_multi_line(r"\.def\((?P<key_space>\s*)\"(?P<key_func>[a-z0-9|_]+)\"", "", path, lines)
         self.py_ops = pattern.re_matcher(
             r"ops\.(?P<key_namespace>[a-z0-9|_|]+)\.(?P<key_op>[a-z0-9|_|]+)",
             lambda match, path, rg:
@@ -73,7 +71,6 @@ class TorchProvider:
             pattern.Ref(key=match.group("key_op"),
                         path=path, range=rg),
             use_search=True)
-                    # torch._C._jit_pass_inline(mod_canonicalized)
         self.py_pybind_func = pattern.re_matcher(
             r"torch\._C\.(?P<key_func>[a-z0-9|_]+)",
             lambda match, path, rg:
@@ -92,8 +89,6 @@ class TorchProvider:
         for generated in generated_cpp:
             if path.endswith(generated):
                 results += self.cpp_generated(path, source, begin, end)
-        for res in results:
-            self.logger.info("Extracted %s from %s, range=%s", res.key, res.path, res.range)
         return results
 
     def _py_extract(self, path, source, begin, end):
@@ -101,8 +96,6 @@ class TorchProvider:
         results += self.py_ops(path, source, begin, end)
         results += self.py_variable_methods(path, source, begin, end)
         results += self.py_pybind_func(path, source, begin, end)
-        for res in results:
-            self.logger.info("Py: Extracted %s from %s", res.key, res.path)
 
         return results
 
