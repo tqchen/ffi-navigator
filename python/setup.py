81d38fc5598454bec322f63cef0ea4ccea0b449e
tqchen <tianqi.tchen@gmail.com>
2020-01-04 10:53:34 -0800

    Release new ver on pypi

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name = "ffi-navigator",
-    version = "0.3.3",
+    version = "0.4.0",
     license="Apache-2.0",
     description = "Language server for navigating FFI calls",
     author = "tvm community",
