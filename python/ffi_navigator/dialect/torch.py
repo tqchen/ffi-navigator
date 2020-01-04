58616f0846d42f4c1fea224ec4ab4338bb3f2665
Masahiro Masuda <masahi129@gmail.com>
2020-01-04 15:18:44 +0900

    update torch provider

--- python/ffi_navigator/dialect/torch.py
+++ python/ffi_navigator/dialect/torch.py
@@ -5,6 +5,7 @@ import numpy as np
 from bisect import bisect
 from .. import pattern
 from ..lsp import Range, Position
+from .base_provider import BaseProvider
 
 
 def _re_match_multi_line(pat, path, lines):
@@ -34,7 +35,7 @@ def _re_match_multi_line(pat, path, lines):
     return result
 
 
-class TorchProvider:
+class TorchProvider(BaseProvider):
     """Provider for Torch FFI.
 
     Parameters
@@ -45,9 +46,7 @@ class TorchProvider:
     logger : Logger object
     """
     def __init__(self, resolver, logger):
-        self.resolver = resolver
-        self._pypath_root = None
-        self.logger = logger
+        super().__init__(resolver, logger, "torch")
         # A pattern for c10 regesterd operators
         # c10::RegisterOperators()
         #   .op("quantized::conv2d",
@@ -101,6 +100,12 @@ class TorchProvider:
                         path=path, range=rg),
             use_search=True)
 
+    def get_additional_scan_dirs(self, root_path):
+        return [
+            os.path.join(root_path, "aten", "src", "ATen"),
+            os.path.join(root_path, "torch")
+        ]
+
     def _cc_extract(self, path, source, begin, end):
         results = self.c10_reg(path, source, begin, end)
         results += self.cpp_pybind_func(path, source)
@@ -121,40 +126,7 @@ class TorchProvider:
         results += self.py_wrapped(path, source, begin, end)
         return results
 
-    def init_pass(self, path, source):
-        """This function will be called for each file before extract."""
-        if path.endswith("torch/__init__.py"):
-            self._pypath_root = os.path.abspath(path[:-len("/__init__.py")])
-            self.resolver.add_package("torch", self._pypath_root)
-            self.logger.info("Torch: find python path %s", self._pypath_root)
-
     def extract(self, path, source, begin=0, end=None):
-        """This function will be called for each file
-        Extract patterns in the file as specified in pattern.py and return them.
-        """
-        if path.endswith(".cpp") and not path.endswith("_test.cpp"):
-            return self._cc_extract(path, source, begin, end)
-        if path.endswith(".py"):
-            return self._py_extract(path, source, begin, end)
+        if not path.endswith("_test.cpp"):
+            return super(TorchProvider, self).extract(path, source, begin, end)
         return []
-
-    def extract_symbol(self, path, source, pos):
-        """Extract possible pattern in the specified location, if not found, return None."""
-        # only search a small context
-        begin = max(pos.line - 1, 0)
-        end = min(pos.line + 2, len(source))
-        # We can use extract and verify to get the pattern.
-        for res in self.extract(path, source, begin, end):
-            if (isinstance(res, (pattern.Ref, pattern.Def)) and
-                res.range.start.line <= pos.line and
-                res.range.end.line >= pos.line and
-                res.range.start.character <= pos.character and
-                res.range.end.character >= pos.character):
-                return res
-        return None
-
-    def get_additional_scan_dirs(self, root_path):
-        return [
-            os.path.join(root_path, "aten", "src", "ATen"),
-            os.path.join(root_path, "torch")
-        ]
